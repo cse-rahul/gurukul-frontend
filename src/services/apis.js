603066ad28bb68a9d8465365c981aa7f60ff9b60
Rahul Maurya <rahulramanmaurya@gmail.com>
2024-09-27 20:54:15 +0530

    backend_hosturl_update

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://gurukul-backend-emma.onrender.com/";
+const BASE_URL = "https://gurukul-backend-emma.onrender.com/api/v1";
+
 //const BASE_URL="http://localhost:4000/api/v1";
 
 // AUTH ENDPOINTS
